cb219e96610764e356aad843c49b9beedb83fea2
KyleMolinari <kyle.molinari22@gmail.com>
2026-01-05 23:44:56 -0800

    initial commit

---
+++ rungeotraining.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+str = "cmd /c "
+
+os.system(str+r"cd 'C:\Users\kylem\OneDrive - McMaster University\Documents\MyProjects\GeoguessrTraining'")
+
+os.system(str+r"streamlit run main.py")
