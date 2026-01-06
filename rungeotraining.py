3ffdf64d911838d585b47d6e58be7ef49d6862a4
Kyle Molinari <65366084+KyleMolinari@users.noreply.github.com>
2026-01-06 00:11:13 -0800

    Delete rungeotraining.py

--- rungeotraining.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-
-str = "cmd /c "
-
-os.system(str+r"cd 'C:\Users\kylem\OneDrive - McMaster University\Documents\MyProjects\GeoguessrTraining'")
-
-os.system(str+r"streamlit run main.py")
